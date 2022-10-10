17bd77cb794869287b3ba83c9cc6abd0ab1f8400
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-10 18:53:31 +0200

    add comment to hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,19 +6,28 @@ import coverMobile from "../public/assets/img/hero-mobile.jpg"
 const Hero = () => {
     return (
         <>
+            {/*  access code */}
             <div className='relative  text-[#fdfbf7] flex flex-col h-screen z-20 w-screen items-center justify-center' >
                 <h3 className='text-xl  drop-shadow-xl flex text-center p-2 luthier-regular' >Ide írhatod a meghívón található QR kód alatti számsort a belépéshez</h3>
                 <input type="text" className='border-2 border-[#fdfbf7] m-2 rounded-xl bg-transparent px-2 ' />
                 <button className='luthier-regular px-2 bg-[#fdfbf7] text-[#ffbd59] rounded-xl'>Küldés</button>
             </div>
+
+            
+
+            {/*  Desktop bcg */}
             <div className="absolute hidden lg:block top-0 left-0 h-screen overflow-y-hidden w-screen ">
                 <Image src={coverDesk} layout="responsive" objectFit='cover'
                     priority />
             </div>
+
+            {/*  Phone bcg */}
             <div className="absolute  lg:hidden top-0 left-0 h-screen overflow-y-hidden w-screen ">
                 <Image src={coverMobile} layout="fill" objectFit='cover'
                     priority />
             </div>
+
+            {/* Overlay */}
             <div className="absolute bg-white/[0.14] z-10 top-0 left-0 h-screen w-screen" ></div>
         </>
     )
