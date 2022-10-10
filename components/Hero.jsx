76e7a1c052350c52a105b63d95afa9f595964f96
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-10 16:02:34 +0200

    add white layer to hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     return (
         <>
             <div className='relative  text-[#fdfbf7] flex flex-col h-screen z-20 w-screen items-center justify-center' >
-                <h3 className='text-xl max-w-[500px] drop-shadow-xl flex text-center p-2 luthier-regular' >Ide írhatod a meghívón található QR kód alatti számsort a belépéshez</h3>
+                <h3 className='text-xl  drop-shadow-xl flex text-center p-2 luthier-regular' >Ide írhatod a meghívón található QR kód alatti számsort a belépéshez</h3>
                 <input type="text" className='border-2 border-[#fdfbf7] m-2 rounded-xl bg-transparent px-2 ' />
                 <button className='luthier-regular px-2 bg-[#fdfbf7] text-[#ffbd59] rounded-xl'>Küldés</button>
             </div>
@@ -19,7 +19,7 @@ const Hero = () => {
                 <Image src={coverMobile} layout="fill" objectFit='cover'
                     priority />
             </div>
-            <div className="absolute bg-black/30 z-10 top-0 left-0 h-screen w-screen" ></div>
+            <div className="absolute bg-white/[0.14] z-10 top-0 left-0 h-screen w-screen" ></div>
         </>
     )
 }
