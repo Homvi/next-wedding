bdc0050782585e841b629fab66765f2777b83249
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-10 12:29:10 +0200

    add luthier font

---
+++ components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <h3 className='luthier-regular' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis accusamus commodi at vel, nulla quisquam expedita perspiciatis aut modi fugit sit eaque voluptas reiciendis unde exercitationem reprehenderit magni nisi delectus!</h3>
+    )
+}
+
+export default Hero
