bb84cc85ca4bbe0bb32d4342d852f2accf7a44f9
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-10 14:27:03 +0200

    add image to landing

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
+import coverDesk from "../public/assets/img/hero-desktop.jpg"
+import coverMobile from "../public/assets/img/hero-mobile.jpg"
 
 const Hero = () => {
     return (
         <>
-            <div className='flex flex-col h-screen w-screen items-center justify-center' >
-                <h3 className='flex text-center p-2 luthier-regular' >Ide írhatod a meghívón található QR kód alatti számsort a belépéshez</h3>
-                <input type="text" className='border-2 m-2' />
+            <div className='relative  text-[#fdfbf7] flex flex-col h-screen z-20 w-screen items-center justify-center' >
+                <h3 className='text-xl max-w-[500px] drop-shadow-xl flex text-center p-2 luthier-regular' >Ide írhatod a meghívón található QR kód alatti számsort a belépéshez</h3>
+                <input type="text" className='border-2 border-[#fdfbf7] m-2 rounded-xl bg-transparent px-2 ' />
             </div>
+            <div className="absolute hidden lg:block top-0 left-0 h-screen overflow-y-hidden w-screen ">
+                <Image src={coverDesk} layout="responsive" objectFit='cover' />
+            </div>
+            <div className="absolute  lg:hidden top-0 left-0 h-screen overflow-y-hidden w-screen ">
+                <Image src={coverMobile} layout="fill" objectFit='cover' />
+            </div>
+            <div className="absolute z-10 bg-white/30 top-0 left-0 h-screen w-screen" ></div>
         </>
     )
 }
