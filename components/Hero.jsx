1ab40468c059865415ed1cb8803951a332c51209
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-11 11:31:59 +0200

    add hero verified version

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import coverDesk from "../public/assets/img/hero-desktop.jpg"
 import coverMobile from "../public/assets/img/hero-mobile.jpg"
 import users from "../data/guests"
+import VerifyedHero from "../components/VerifyedHero"
 
 const Hero = () => {
     //must to save user to localStore
@@ -10,9 +11,10 @@ const Hero = () => {
     const [accessCode, setAccessCode] = useState("")
     const [actualUser, setActualUser] = useState("")
     const [isError, setIsError] = useState(false)
+    const [isCounterVisible, setIsCounterVisible] = useState(false)
 
     const handleSubmit = () => {
-        
+        setIsCounterVisible(!isCounterVisible)
     }
 
     return (
@@ -21,14 +23,17 @@ const Hero = () => {
             <nav className='absolute w-screen z-30 top-0 flex justify-end' >{actualUser && <div className='luthier-regular italic text-[#fdfbf7] drop-shadow-lg  p-1' >Üdv, {actualUser}</div>}</nav>
 
             {/*  access code */}
-            <div className='relative  text-[#fdfbf7] flex flex-col h-screen z-20 w-screen items-center justify-center' >
+
+            {isCounterVisible ? <VerifyedHero /> : (<div className='relative  text-[#fdfbf7] flex flex-col h-screen z-20 w-screen items-center justify-center' >
                 <h3 className='text-xl max-w-[500px]  drop-shadow-xl flex text-center p-2 luthier-regular' >Ide írhatod a meghívón található QR kód alatti számsort a belépéshez</h3>
                 <input value={accessCode} onChange={e => setAccessCode(e.target.value)} type="text" className='border-4 border-[#fdfbf7] m-2 rounded-xl bg-transparent px-2 ' />
                 {/* error message */}
                 <div className={isError ? "text-red-600 bg-white rounded-lg px-1 max-w-[500px]  border-red-500 border-2 m-1 text-center" : "text-red-600 bg-white rounded-lg px-1 max-w-[500px]  border-red-500 hidden border-2 m-1 text-center"}>A megadott számsor nem tartozik egyetlen vendéghez sem!</div>
                 {/* submit btn */}
                 <button onClick={handleSubmit} className='luthier-regular px-2 bg-[#fdfbf7] text-[#ffbd59] rounded-xl'>Küldés</button>
-            </div>
+            </div>)}
+
+
 
             {/* hero after identification */}
 
---
+++ components/VerifyedHero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const VerifyedHero = () => {
+    return (
+        <div className='relative  text-[#fdfbf7] flex flex-col h-screen z-20 w-screen items-center justify-center' >
+            <h3 className=' max-w-[500px]  drop-shadow-xl parisienne text-5xl mb-3 flex text-center p-2 luthier-regular' >Brigi & Kálmi</h3>
+            <div className="felx text-3xl luthier-regular justify-center">2023.08.26</div>
+            <div className="felx justify-center prata">
+                <span>130:</span>
+                <span>23:</span>
+                <span>48:</span>
+                <span>11</span>
+            </div>
+        </div>
+    )
+}
+
+export default VerifyedHero
