bffe750d85e58bdf21cf8fd4ae10ee185b2a7a47
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-11 12:29:30 +0200

    change timestampt to correct date

--- components/VerifyedHero.jsx
+++ components/VerifyedHero.jsx
@@ -6,7 +6,7 @@ const VerifyedHero = () => {
         <div className='relative  text-[#fdfbf7] flex flex-col h-screen z-20 w-screen items-center justify-center' >
             <h3 className=' max-w-[500px]  drop-shadow-xl parisienne text-5xl mb-3 flex text-center p-2 luthier-regular' >Brigi & Kálmi</h3>
             <div className="felx text-3xl luthier-regular justify-center">2023.08.26</div>
-            <CountdownTimer countdownTimerstampMs={1665741300000} />
+            <CountdownTimer countdownTimerstampMs={1693045620000} />
         </div>
     )
 }
