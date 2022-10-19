6763d97af39f7261ad92aae38118ef178b7bda92
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-19 11:39:11 +0200

    import react icons

--- components/WrongAccesCode.jsx
+++ components/WrongAccesCode.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {AiOutlineClose} from 'react-icons/ai'
 
 const WrongAccesCode = () => {
     return (
         <div className='relative luthier-regular max-w-[400px] rounded-3xl m-3  p-1 text-[#ff0000] bg-white/60' >
-            <div className=" absolute right-4 top-2 flex justify-end">x</div>
+            <div className=" absolute right-4 top-2 flex justify-end"><AiOutlineClose/></div>
             <div className="flex text-center text-lg py-6 px-2">
                 Sajnos helytelenül írtad be a számsort,<br/> kérlek próbáld meg újra!
             </div>
