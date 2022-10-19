8edf4f98331dabb007b65dd66ec1f9b0a71c679e
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-19 11:34:05 +0200

    add content to AccessCodeAlert

--- components/WrongAccesCode.jsx
+++ components/WrongAccesCode.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const WrongAccesCode = () => {
-  return (
-    <div>WrongAccesCode</div>
-  )
+    return (
+        <div className='relative luthier-regular max-w-[400px] rounded-3xl m-3  p-1 text-[#ff0000] bg-white/60' >
+            <div className=" absolute right-4 top-2 flex justify-end">x</div>
+            <div className="flex text-center text-lg py-6 px-2">
+                Sajnos helytelenül írtad be a számsort,<br/> kérlek próbáld meg újra!
+            </div>
+        </div>
+    )
 }
 
 export default WrongAccesCode
