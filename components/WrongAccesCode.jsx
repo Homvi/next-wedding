8f68f4b7b84b8597e7e40e8f442b3d12166bc08d
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-19 13:16:04 +0200

    change cursor to poiner at error message

--- components/WrongAccesCode.jsx
+++ components/WrongAccesCode.jsx
@@ -4,8 +4,8 @@ import {AiOutlineClose} from 'react-icons/ai'
 const WrongAccesCode = () => {
     return (
         <div className='relative luthier-regular max-w-[400px] rounded-3xl m-3  p-1 text-[#ff0000] bg-white/60' >
-            <div className=" absolute right-4 top-2 flex justify-end"><AiOutlineClose/></div>
-            <div className="flex text-center text-lg py-6 px-2">
+            <div className=" absolute cursor-pointer right-4 top-2 flex justify-end"><AiOutlineClose/></div>
+            <div className="flex text-center text-lg py-6 px-4">
                 Sajnos helytelenül írtad be a számsort,<br/> kérlek próbáld meg újra!
             </div>
         </div>
