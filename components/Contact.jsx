800dbc4a5d5cdf55eaec7f42fedd3750aba143d2
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-21 12:37:31 +0200

    change form name

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -13,25 +13,31 @@ const Contact = () => {
                     />
                 </div>
                 {/* form */}
-                <form className='justify-around items-start text-white luthier-regular  flex w-[50%] flex-col' data-netlify="true" name="visszajelzesek" action="/success" method="POST">
+                <form className='justify-around items-start text-white luthier-regular  flex w-[50%] flex-col' netlify-honeypot="bot-field" data-netlify="true" name="contact" action="/success" method="POST">
+                    <input type="hidden" name="contact" value="contact" />
+                    <label className="hidden">
+                        Don’t fill this out if you’re human:
+                        <input name="bot-field" />
+                    </label>
+
                     <p className='flex justify-start  flex-col pl-5 w-[90%]' >
                         <label htmlFor="canCome" className='text-left pb-2' >
                             Kérlek itt jelezz vissza, hogy tudsz e jönni.
                         </label>
-                        <input className='border-4 border-[#ffbd59]/80 px-2  rounded-xl bg-transparent' type="text" name="tud jönni?" id="canCome" />
+                        <input className='border-4 border-[#ffbd59]/80 px-2  rounded-xl bg-transparent' type="text" name="canCome" id="canCome" />
                     </p>
 
                     <p className='flex justify-start flex-col pl-5 w-[90%]' >
                         <label htmlFor="howManyPerson" className='text-left pb-2' >
                             Hányan jöttök?
                         </label>
-                        <input className='border-4 border-[#ffbd59]/80 px-2  rounded-xl bg-transparent' type="text" placeholder="fő, név..." name="Hanyan jottok?" id="howManyPerson" />
+                        <input className='border-4 border-[#ffbd59]/80 px-2  rounded-xl bg-transparent' type="text" placeholder="fő, név..." name="howManyPerson" id="howManyPerson" />
                     </p>
                     <p className='flex justify-start flex-col pl-5 w-[90%]' >
                         <label htmlFor="foodIntolerance" className='text-left  pb-2' >
                             Írd meg, hogy van e ételérzékenységed.
                         </label>
-                        <input className='border-4 border-[#ffbd59]/80 px-2  rounded-xl bg-transparent' name="etel erzekenyseg" id="foodIntolerance"></input>
+                        <input className='border-4 border-[#ffbd59]/80 px-2  rounded-xl bg-transparent' name="foodIntolerance" id="foodIntolerance"></input>
                     </p>
                     <p className='felx justify-center w-[100%]' >
                         <button className='luthier-bold px-3 bg-[#fdfbf7] text-lg text-[#ffbd59] rounded-xl m-3 ' type="submit">Küldés</button>
