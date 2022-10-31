d40849fe6de67f8a03e08cb30de99db8e601d6be
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-31 17:45:02 +0100

    plan back to normal position

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -99,10 +99,10 @@ const Hero = () => {
             {/* plan */}
 
 
-            {authState === "authorized" && <Plan />}
             {authState === "authorized" && <Intro />}
             {authState === "authorized" && <OurStory />}
             {authState === "authorized" && <OurLove />}
+            {authState === "authorized" && <Plan />}
             {authState === "authorized" && <ContactInfo />}
             {authState === "authorized" && <Providers />}
             {/*   {authState === "authorized" && <Contact user={actualUser} />} */}
