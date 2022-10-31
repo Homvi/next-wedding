cd499cf2dc40c6c0cd632524654452fb1fda5c73
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-31 09:30:59 +0100

    add tracking to intro

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -6,13 +6,13 @@ const Intro = () => {
     return (
         <section className=' py-3 min-h-screen items-center  text-center flex flex-col' >
             <div className="flex w-[90%] custom-border h-screen items-center">
-                <div className="text-[#fdfbf7] flex text-lg md:text-2xl justify-center items-center text-center h-[60vh] md:h-[90vh] md:w-[50%] md:p-10 p-1 luthier-bold-italic">
+                <div className="text-[#fdfbf7] flex tracking-widest leading-10 text-2xl items-center text-center h-[60vh] md:h-[90vh] md:w-[50%] md:p-10 p-1 luthier-bold-italic">
                     Minden kisfiú és kislány megálmodja milyen lesz, ha egyszer neki is eljön a Nagy nap!
                     A mi Nagy napunk 2023. augusztus 26.-a.
                     Osztozzatok örömünkben mikor örök hűséget fogadunk egymásnak!
                     Szeretettel várunk benneteket!
                 </div>
-                <div className="relative hidden mx-auto md:block overflow-y-hidden w-[60vh] p-10 ">
+                <div className="relative hidden rounded-sm mx-auto md:block overflow-y-hidden w-[60vh]">
                     <Image src={holdingHands} layout="responsive" objectFit='cover'
                     />
                 </div>
