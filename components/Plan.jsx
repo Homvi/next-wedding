b0f48be9412c915f2dd33700c0106843c86dd8b2
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-31 17:17:33 +0100

    add icons to plan

--- components/Plan.jsx
+++ components/Plan.jsx
@@ -1,4 +1,17 @@
+import Image from 'next/image'
+import churc from "../public/assets/icon/churc.svg"
+import maiden from "../public/assets/icon/lanykikeres.svg"
+import dance from "../public/assets/icon/menyecsketanc.svg"
+import cake from "../public/assets/icon/torta.svg"
+import dinner from "../public/assets/icon/vacsora.svg"
+import guest from "../public/assets/icon/vendegvaras.svg"
+import civil from "../public/assets/icon/polgari.svg"
+
+
+
 const Plan = () => {
+
+
 
     const adresses = {
         maidenRequest: "https://www.google.com/maps/place/Sereg%C3%A9lyes,+Ady+Endre+u.+99,+8111+Magyarorsz%C3%A1g/@47.1077026,18.5724881,17z/data=!3m1!4b1!4m5!3m4!1s0x4769fc2cf59e26ef:0xaaf0c6e6e6708967!8m2!3d47.107699!4d18.5746768",
@@ -13,40 +26,54 @@ const Plan = () => {
                 <h1 className='uppercase text-4xl tracking-wider mt-3 mb-10 luthier-bold ' >Menetrend</h1>
 
                 {/* maiden request */}
-                <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >13:00</span> Lánykikérés</p>
+                <div className="flex items-baseline">
+                    <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >13:00</span> Lánykikérés</p><span className='w-[20px] m-2' ><Image src={maiden} /></span>
+                </div>
                 <p className='luthier-italic  text-center ' ><a target="_blank" href={adresses.maidenRequest} rel="noopener noreferrer">8111 Seregélyes Ady Endre utca 99</a> </p>
 
                 <div id="hr" className='w-[4px] h-[4px] bg-[#fff] m-4 rounded-full'></div>
 
                 {/* churc cremony */}
-                <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >14:00</span> Templomi szertartás</p>
+                <div className="flex items-baseline">
+                    <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >14:00</span >  Templomi szertartás</p><span className='w-[20px] m-2' ><Image src={churc} /></span>
+                </div>
                 <p className='luthier-italic text-center ' ><a target="_blank" href={adresses.churcCremony} rel="noopener noreferrer">Budai úti Református Templom 8000 Székesfehérvár Budai út 113.</a> </p>
 
                 <div id="hr" className='w-[4px] h-[4px] bg-[#fff] m-4 rounded-full'></div>
 
                 {/* wait guest */}
-                <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >16:30</span> Vendégvárás</p>
+                <div className="flex items-baseline">
+                    <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >16:30</span> Vendégvárás</p><span className='w-[20px] m-2' ><Image src={guest} /></span>
+                </div>
                 <p className='luthier-italic text-center ' > <a target="_blank" href={adresses.waitGuest} rel="noopener noreferrer">Sokoró Fogadó 9112 Sokorópátka Főmajor</a></p>
 
                 <div id="hr" className='w-[4px] h-[4px] bg-[#fff] m-4 rounded-full'></div>
 
                 {/* civil */}
-                <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >17:30</span> Polgári szertartás</p>
+                <div className="flex items-baseline">
+                    <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >17:30</span> Polgári szertartás</p><span className='w-[20px] m-2' ><Image src={civil} /></span>
+                </div>
                 <p className='luthier-italic text-center' >Pavilon</p>
 
                 <div id="hr" className='w-[4px] h-[4px] bg-[#fff] m-4 rounded-full'></div>
 
                 {/* Dinner */}
-                <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >19:30</span> Vacsora</p>
+                <div className="flex items-baseline">
+                    <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >19:30</span> Vacsora</p><span className='w-[20px] m-2' ><Image src={dinner} /></span>
+                </div>
 
                 <div id="hr" className='w-[4px] h-[4px] bg-[#fff] m-4 rounded-full'></div>
 
                 {/* Cake */}
-                <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >22:30</span> Menyasszonyi torta</p>
+                <div className="flex items-baseline">
+                    <p className=' text-2xl luthier-bold-italic' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >22:30</span> Menyasszonyi torta</p><span className='w-[20px] m-2' ><Image src={cake} /></span>
+                </div>
 
                 <div id="hr" className='w-[4px] h-[4px] bg-[#fff] m-4 rounded-full'></div>
                 {/* Dance */}
-                <p className=' text-2xl luthier-bold-italic mb-5' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >00:00</span> Menyecsketánc</p>
+                <div className="flex items-baseline">
+                    <p className=' text-2xl luthier-bold-italic mb-5' ><span className='text-[#ffbd59] text-2xl luthier-bold-italic' >00:00</span> Menyecsketánc</p><span className='w-[20px] m-2' ><Image src={dance} /></span>
+                </div>
             </div>
 
         </section>
