c1242c27e67a47ab0b6559bc94467bb4168a33c2
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-31 09:55:30 +0100

    add ruler to top of intro

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -4,15 +4,15 @@ import Image from 'next/image'
 
 const Intro = () => {
     return (
-        <section className=' py-3 min-h-screen items-center  text-center flex flex-col' >
-            <div className="flex w-[90%] custom-border h-screen items-center">
-                <div className="text-[#fdfbf7] flex tracking-widest leading-10 text-2xl items-center text-center h-[60vh] md:h-[90vh] md:w-[50%] md:p-10 p-1 luthier-bold-italic">
+        <section className=' py-3 max-h-screen items-center  text-center flex flex-col' >
+            <div className="flex w-[90%] custom-border custom-border-top p-5 items-center">
+                <div className="text-[#fdfbf7] flex tracking-widest leading-10 text-2xl items-center text-center md:h-[90vh] md:w-[50%] md:p-10 p-1 luthier-bold-italic">
                     Minden kisfiú és kislány megálmodja milyen lesz, ha egyszer neki is eljön a Nagy nap!
                     A mi Nagy napunk 2023. augusztus 26.-a.
                     Osztozzatok örömünkben mikor örök hűséget fogadunk egymásnak!
                     Szeretettel várunk benneteket!
                 </div>
-                <div className="relative hidden rounded-sm mx-auto md:block overflow-y-hidden w-[60vh]">
+                <div className="relative hidden rounded-sm mx-auto md:block overflow-y-hidden w-[55vh]">
                     <Image src={holdingHands} layout="responsive" objectFit='cover'
                     />
                 </div>
