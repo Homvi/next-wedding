ef85b8870af031010131d87049feac52067afbfc
HonvedoAdam <honvedoadamkj@gmail.com>
2022-11-06 20:56:14 +0100

    add guest code

--- data/guests.js
+++ data/guests.js
@@ -1,6 +1,7 @@
 let guests = [
   { firstName: "Eszter", lastName: "Honvédő", accessCode: 1111 },
   { firstName: "Ádám", lastName: "Honvédő", accessCode: 2222 },
+  { firstName: "Vendég", lastName: "Vendég", accessCode: 20230826 },
 ];
 
 export default guests;
