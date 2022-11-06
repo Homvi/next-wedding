ff48105864db9100333c80b9d7fc63697c1abf45
HonvedoAdam <honvedoadamkj@gmail.com>
2022-11-06 16:34:53 +0100

    add more flower

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import holdingHands from "../public/assets/img/holdingHands.jpg"
 import Image from 'next/image'
+import flower1 from "../public/assets/icon/white/virag1.svg"
+import flower2 from "../public/assets/icon/white/virag2.svg"
+import flower1Green from "../public/assets/icon/green/virag1.svg"
+import flower2Green from "../public/assets/icon/green/virag2.svg"
 
 const Intro = () => {
     return (
-        <section className=' py-10 max-h-[100vh] box-border max-w-[95vw] border-b-4 border-[#ffbd59] md:border-[#faf8ebc3] items-center justify-center text-center flex flex-col' >
+        <section className=' py-10 relative max-h-[100vh] box-border max-w-[95vw] border-b-4 border-[#ffbd59] md:border-[#faf8ebc3] items-center justify-center text-center flex flex-col' >
+            {/* flowers for flip scale-x-[-1] */}
+            {/* dekstop */}
+            <span className='w-[80px] m-2 hidden md:block absolute bottom-0 left-[-35px] sm:left-[-75px] sm:w-[110px]' ><Image src={flower1} /></span>
+            <span className='w-[80px] m-2 hidden md:block absolute rotate-[330deg] top-[30px] right-[-35px] sm:right-[-85px] sm:w-[150px]' ><Image src={flower2} /></span>
+            {/* mobile */}
+            <span className='w-[60px] m-2 absolute md:hidden bottom-0 left-[-35px] sm:left-[-60px] sm:w-[110px]' ><Image src={flower1Green} /></span>
+            <span className='w-[60px] m-2 absolute md:hidden rotate-[330deg] top-[30px] right-[-35px] sm:right-[-75px] sm:w-[150px]' ><Image src={flower2Green} /></span>
             <div className="flex w-[100%]  min-h-[80vh]  p-5 items-center">
                 <div className=" flex tracking-wider md:tracking-widest leading-10 text-2xl items-center text-center md:h-[90vh] md:w-[60%] md:p-10 p-1 luthier-bold-italic">
                     Minden kisfiú és kislány megálmodja milyen lesz, ha egyszer neki is eljön a Nagy nap!
--- components/OurLove.jsx
+++ components/OurLove.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 
 const OurLove = () => {
     return (
-        <section className=' py-5 w-[95vw] min-h-screen border-b-4 border-[#ffbd59] md:border-[#faf8ebc3] items-center  text-center flex flex-col' >
-            <div className="flex w-[100%] p-5 items-center">
+        <section className=' py-5 w-[95vw] min-h-screen border-b-4 border-[#ffbd59] md:border-[#faf8ebc3] items-center  justify-center text-center flex flex-col' >
+            <div className="flex w-[100%] p-5  items-center">
                 <div className="flex tracking-wider md:tracking-widest leading-10 text-2xl items-center text-center md:h-[90vh] md:w-[60%] luthier-bold-italic">
                     Az a szerelem mely életünk során egyszer adódik meg igazán. Szerelmünk első percétől kezdve tudtuk, hogy mi ketten egy családot szeretnénk alkotni melyben a családtagjaink és barátaink is részesei lesznek.
                     Ezért tehát összekötjük életünket meg alapozva családunk bővülését. Bízva abba, hogy együtt éljük le életünket sok élménnyel és örömmel gazdagodva.
