1c61910d27a529d656a1a11acad391598b461707
HonvedoAdam <honvedoadamkj@gmail.com>
2022-12-04 10:44:57 +0100

    change heading in providers to luthier

--- components/Providers.jsx
+++ components/Providers.jsx
@@ -5,7 +5,7 @@ import MyCarousel from "./MyCarousel"
 const Providers = () => {
   return (
     <section className='px-1 mx-auto w-full' >
-      <h1 className='mt-3 mb-3 flex justify-center text-3xl md:text-4xl text-center tracking-wider uppercase luthier' >Szolgáltatóink</h1>
+      <h1 className='mt-5 mb-3 flex justify-center text-3xl md:text-4xl text-center tracking-wider uppercase luthier-regular' >Szolgáltatóink</h1>
       <MyCarousel />
     </section>
   )
