fa82244cb7b895c1176d0b5e74640ded852fc1d4
HonvedoAdam <honvedoadamkj@gmail.com>
2022-12-04 10:34:44 +0100

    implement providers slider

---
+++ components/MyCarousel.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import Carousel from 'react-material-ui-carousel'
+import Image from 'next/image';
+
+function MyCarousel(props) {
+    var items = [
+        {
+            src: "/../public/assets/img/eszter.jpg",
+            width: 1365,
+            height: 2048,
+            name: "Honvédő Eszter - fotó",
+        },
+        {
+            width: 764,
+            height: 1021,
+            src: "/../public/assets/img/bestMan.jpg",
+            name: "Pap Ferenc - vőfély",
+        },
+        {
+            width: 750,
+            height: 500,
+            src: "/../public/assets/img/band.jpg",
+            name: "Zé Klub Zenekar - zene",
+        },
+        {
+            width: 5520,
+            height: 3680,
+            src: "/../public/assets/img/photo.jpg",
+            name: "Zengő Ferenc - fotó",
+        },
+        {
+            width: 5184,
+            height: 3456,
+            src: "/../public/assets/img/video.jpeg",
+            name: "KuriMedia - videó",
+        },
+        {
+            width: 990,
+            height: 660,
+            src: "/../public/assets/img/sokoro.jpg",
+            name: "Sokoró Fogadó - lakodalmas helyszín",
+        },
+        {
+            width: 1800,
+            height: 1200,
+            src: "/../public/assets/img/sokoroHouse.jpg",
+            name: "Sokoró Fogadó - lakodalmas helyszín",
+        },
+    ]
+
+    return (
+        <Carousel
+            navButtonsAlwaysVisible={false}
+        >
+            {
+                items.map((item, i) => <Item key={i} item={item} />)
+            }
+        </Carousel>
+    )
+}
+
+function Item(props) {
+    return (
+        <div className='relative flex align-center justify-center flex-col max-w-[500px] min-h-[60vh] mx-auto'>
+            <div className="flex justify-center luthier-bold-italic text-lg align-center">
+                <h2 className='text-center p-1 h-10'>{props.item.name}</h2>
+            </div>
+            <div className="image-container justify-center flex m-2 h-[400px]">
+                <Image objectFit='contain' alt={props.item.name} className={props.item.height >= props.item.width ? ' border-red-400' : "  border-blue-700"} height={props.item.height} width={props.item.width} src={props.item.src} />
+            </div>
+        </div>
+    )
+}
+
+export default MyCarousel;
--- components/Providers.jsx
+++ components/Providers.jsx
@@ -1,111 +1,12 @@
-import Image from 'next/image'
 import React from 'react'
-import photo from "../public/assets/img/photo.jpg"
-import video from "../public/assets/img/video.jpeg"
-import bestMan from "../public/assets/img/bestMan.jpg"
-import sokoro from "../public/assets/img/sokoro.jpg"
-import sokoroHouse from "../public/assets/img/sokoroHouse.jpg"
-import band from "../public/assets/img/band.jpg"
-import eszter from "../public/assets/img/eszter.jpg"
+import MyCarousel from "./MyCarousel"
+
 
 const Providers = () => {
   return (
-    <section className='border-b-4 py-5 border-[#ffbd59] md:border-[#faf8ebc3]  min-h-[50vh] items-center w-[95vw]  text-center flex flex-col' >
-      <h1 className='mt-3 mb-10 text-3xl md:text-4xl tracking-wider uppercase luthier-bold' >Szolgáltatóink</h1>
-      <div className="gap-5 grid w-full grid-cols-2 xl:grid-cols-3 p-2 xl:p-0">
-
-        {/* grid element photo eszter*/}
-     {/*    <div className=" w-full">
-          <div className="image-container flex justify-center items-center relative  h-fit w-[100%]">
-            <Image
-              alt='photo'
-              src={eszter}
-              objectFit="contain"
-              widt={500}
-            />
-          </div>
-          <div className="text-center flex items-end justify-center luthier-bold-italic">Honvédő Eszter - fotós, webdesigner</div>
-        </div> */}
-
-        {/* grid element photo*/}
-       {/*  <div className=" w-full">
-          <div className="image-container flex justify-center items-center relative  h-fit w-[100%]">
-            <Image
-              alt='photo'
-              src={photo}
-              objectFit="contain"
-              widt={500}
-            />
-          </div>
-          <div className="text-center flex items-end justify-center luthier-bold-italic">Zengő Ferenc - fotó</div>
-        </div> */}
-
-
-        {/* grid element video*/}
-      {/*   <div className=" w-full">
-          <div className="image-container flex justify-center items-center relative  h-fit w-[100%]">
-            <Image
-              alt='video'
-              src={video}
-              objectFit="contain"
-              widt={500}
-            />
-          </div>
-          <div className="text-center flex items-end justify-center luthier-bold-italic">KuriMedia - videó</div>
-        </div> */}
-
-        {/* grid element best man */}
-      {/*   <div className=" w-full">
-          <div className="image-container flex justify-center items-center relative  h-fit w-[100%]">
-            <Image
-              alt='best Man'
-              src={bestMan}
-              objectFit="contain"
-              height={507}
-            />
-          </div>
-          <div className="text-center flex items-end justify-center luthier-bold-italic">Pap Ferenc - vőfély</div>
-        </div> */}
-
-        {/* grid element Band*/}
-      {/*   <div className=" w-full">
-          <div className="image-container flex justify-center items-center relative  h-fit w-[100%]">
-            <Image
-              alt='Band'
-              src={band}
-              objectFit="contain"
-              widt={500}
-            />
-          </div>
-          <div className="text-center flex items-end justify-center luthier-bold-italic">Zé Klub zenekar - zene</div>
-        </div> */}
-
-        {/* grid element sokoro*/}
-      {/*   <div className=" w-full">
-          <div className="image-container flex justify-center items-center relative  h-fit w-[100%]">
-            <Image
-              alt='sokoro'
-              src={sokoro}
-              objectFit="contain"
-              widt={500}
-            />
-          </div>
-          <div className="text-center flex items-end justify-center luthier-bold-italic">Sokoró fogadó - Lakodalmas helyszín</div>
-        </div> */}
-
-        {/* grid element sokoro house*/}
-      {/*   <div className=" w-full">
-          <div className="image-container flex justify-center items-center relative  h-fit w-[100%]">
-            <Image
-              alt='sokoro'
-              src={sokoroHouse}
-              objectFit="contain"
-              widt={500}
-            />
-          </div>
-          <div className="text-center flex items-end justify-center luthier-bold-italic">Sokoró fogadó - Lakodalmas helyszín</div>
-        </div> */}
-      </div>
+    <section className='px-1 mx-auto w-full' >
+      <h1 className='mt-3 mb-3 flex justify-center text-3xl md:text-4xl text-center tracking-wider uppercase luthier' >Szolgáltatóink</h1>
+      <MyCarousel />
     </section>
   )
 }
