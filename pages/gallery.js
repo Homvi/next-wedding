0c970c7736989063bc054f4df1b87b2bb06a554e
HonvedoAdam <honvedoadamkj@gmail.com>
2022-12-18 11:35:47 +0100

    modify gallery font and background

--- pages/gallery.js
+++ pages/gallery.js
@@ -4,13 +4,13 @@ import Modal from "../components/Modal";
 
 const gallery = () => {
     return (
-        <div className="bg-white h-screen">
+        <div className="text-[#62622a] bg-[#faf8eb] md:bg-[#62622a]/70 md:text-[#fdfbf7] ">
             <Head>
                 <title>Esküvői galéria</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <h1 className="text-center text-5xl p-5 text-[#333] Parisienne-Regular ">Brigi & Kálmi</h1>
-            <h1 className="text-center text-4xl p-1 text-[#333] Parisienne-Regular ">Galéria</h1>
+            <h1 className="text-center text-3xl md:text-4xl p-5  luthier-regular uppercase tracking-wider">Brigi & Kálmi</h1>
+            <h1 className="text-center text-2xl md:text-3xl p-1  luthier-regular tracking-wider ">Galéria</h1>
             <Gallery />
             <Modal />
         </div>
